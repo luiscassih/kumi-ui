74a1a9fc85f5c58f25a5aeda0c269a29d22837a3
Luis C <luiscassih@gmail.com>
2026-07-01 19:29:06 -0300

    trying to divide into packages

---
+++ packages/core/src/frameworks/react/index.tsx
@@ -0,0 +1,3 @@
+import type { Composable } from "../../core/builder"
+
+export default const Render = (node: Composable): React
---
+++ packages/react/index.tsx
@@ -0,0 +1,4 @@
+export const Hello = () => {
+  return <h1>Hello from React</h1>
+}
+export default Hello
